5d5993c82ada98ad39e962d7efe592ab6311970b
nilsonamaral <nilsonamaral7@gmail.com>
2023-03-27 21:59:51 -0300

    aula1

---
+++ ALURA-STICKERS/App.java
@@ -0,0 +1,25 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //criar a requisão
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // Extrair dados que serão usados
+        var parser = new jsonParser();
+        List<Map<String, String>> listaDeFilmes;
+    }
+}
