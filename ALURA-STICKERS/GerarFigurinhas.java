0a33f54092cb0732c263a36c1f74334c1fafe867
nilsonamaral <nilsonamaral7@gmail.com>
2023-03-28 20:55:20 -0300

    inicio aula 2

---
+++ ALURA-STICKERS/GerarFigurinhas.java
@@ -0,0 +1,11 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class GerarFigurinhas {
+    
+    public void cria() throws Exception{
+        //Leitura da Imagem 
+        BufferedImage imagemOriginal = ImageIO.read(new File(pathname: "imagem/Top250Movies_1.jpg"));
+    }
+}
