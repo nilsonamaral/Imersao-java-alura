1fa351ff60613a51195a694780fb9df1b55f62cb
nilsonamaral <nilsonamaral7@gmail.com>
2023-03-28 20:32:02 -0300

    finalizado aula 1

--- ALURA-STICKERS/App.java
+++ ALURA-STICKERS/App.java
@@ -16,10 +16,27 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+        
 
         // Extrair dados que serão usados
         var parser = new jsonParser();
-        List<Map<String, String>> listaDeFilmes;
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        // Manipular os dados desejados
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println("\u001b[1mTítulo:\u001b[m " +  filme.get("title"));
+            System.out.println("\u001b[1mURL da Imagen:\u001b[m " + filme.get("image"));
+            System.out.println("\u001b[3m \u001b[104m imDb Rating: \u001b[m" + filme.get("imDbRating"));
+            double classificacao = Double.parseDouble(filme.get("rank"));
+            int estrelas = (int) classificacao;
+
+            for (int i = 1; i <= estrelas; i++) {
+                System.out.print("⭐");
+            }
+
+            System.out.println("\n");
+
+        }
     }
 }
